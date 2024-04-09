f47adccbe4856c7d94b20eeb85255c181125fd4e
Nguyen Cong Hoan <157735901+HoanTheBoy@users.noreply.github.com>
2024-04-10 00:06:49 +0700

    sua lai game main

--- Team code/js/game_main.js
+++ Team code/js/game_main.js
@@ -1,41 +1,10 @@
-let slider = document.querySelector(".slider .list");
-let items = document.querySelectorAll(".slider .list .item");
-let next = document.getElementById("next");
-let prev = document.getElementById("prev");
-let dots = document.querySelectorAll(".slider .dots li");
-
-let lengthItems = items.length - 1;
-let active = 0;
-next.onclick = function () {
-  active = active + 1 <= lengthItems ? active + 1 : 0;
-  reloadSlider();
-};
-prev.onclick = function () {
-  active = active - 1 >= 0 ? active - 1 : lengthItems;
-  reloadSlider();
-};
-let refreshInterval = setInterval(() => {
-  next.click();
-}, 3000);
-function reloadSlider() {
-  slider.style.left = -items[active].offsetLeft + "px";
-  //
-  let last_active_dot = document.querySelector(".slider .dots li.active");
-  last_active_dot.classList.remove("active");
-  dots[active].classList.add("active");
-
-  clearInterval(refreshInterval);
-  refreshInterval = setInterval(() => {
-    next.click();
-  }, 3000);
-}
-
-dots.forEach((li, key) => {
-  li.addEventListener("click", () => {
-    active = key;
-    reloadSlider();
+$(".custom-carousel").owlCarousel({
+  autoWidth: true,
+  loop: true,
+});
+$(document).ready(function () {
+  $(".custom-carousel .item").click(function () {
+    $(".custom-carousel .item").not($(this)).removeClass("active");
+    $(this).toggleClass("active");
   });
 });
-window.onresize = function (event) {
-  reloadSlider();
-};
