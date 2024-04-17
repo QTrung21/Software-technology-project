c835074180486c671075d54aa7a8eb9e36e8ce07
Nguyen Cong Hoan <157735901+HoanTheBoy@users.noreply.github.com>
2024-04-17 23:32:44 +0700

    update_nav_arrow

--- Team code/js/game_main.js
+++ Team code/js/game_main.js
@@ -1,21 +1,34 @@
 $(document).ready(function() {
+  // Initialize the Owl Carousel
+  var owl = $('.custom-carousel').owlCarousel({
+      autoWidth: true,
+      loop: true,
+      dots: false,
+      nav: true, 
+      margin: 10
+  });
+
+  // Event listener for custom previous button
+  $('.owl-prev').click(function() {
+      owl.trigger('prev.owl.carousel');
+  });
+
+  // Event listener for custom next button
+  $('.owl-next').click(function() {
+      owl.trigger('next.owl.carousel');
+  });
+
+  // Add double click event to open links in a new tab
   $('.custom-carousel .item').dblclick(function() {
-    var link = $(this).data('link');
-    if (link) {
-      window.open(link, '_blank');
-    }
+      var link = $(this).data('link');
+      if (link) {
+          window.open(link, '_blank');
+      }
   });
 
-  $('.custom-carousel').owlCarousel({
-    autoWidth: true,
-    loop: true,
-    dots: false,
-    nav: true,
-    navText: ['<span class="owl-prev"><i class="fa-solid fa-less-than"></i></span>', '<span class="owl-next"><i class="fa-solid fa-greater-than"></i></span>']
-  });
-
+  // Add click event to toggle 'active' class
   $(".custom-carousel .item").click(function() {
-    $(".custom-carousel .item").not($(this)).removeClass("active");
-    $(this).toggleClass("active");
+      $(".custom-carousel .item").not(this).removeClass("active");
+      $(this).toggleClass("active");
   });
 });
