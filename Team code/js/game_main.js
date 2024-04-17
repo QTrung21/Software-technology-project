aca0ff06da542bddf9c37bae07908570b6a01b64
Nguyễn Công Hoàn <157735901+HoanTheBoy@users.noreply.github.com>
2024-04-17 23:35:53 +0700

    Update game_main.js

--- Team code/js/game_main.js
+++ Team code/js/game_main.js
@@ -1,21 +1,29 @@
 $(document).ready(function() {
-  $('.custom-carousel .item').dblclick(function() {
-    var link = $(this).data('link');
-    if (link) {
-      window.open(link, '_blank');
-    }
+  var owl = $('.custom-carousel').owlCarousel({
+      autoWidth: true,
+      loop: true,
+      dots: false,
+      nav: true, 
+      margin: 10
   });
 
-  $('.custom-carousel').owlCarousel({
-    autoWidth: true,
-    loop: true,
-    dots: false,
-    nav: true,
-    navText: ['<span class="owl-prev"></span>', '<span class="owl-next"></span>']
+  $('.owl-prev').click(function() {
+      owl.trigger('prev.owl.carousel');
+  });
+
+  $('.owl-next').click(function() {
+      owl.trigger('next.owl.carousel');
+  });
+
+  $('.custom-carousel .item').dblclick(function() {
+      var link = $(this).data('link');
+      if (link) {
+          window.open(link, '_blank');
+      }
   });
 
   $(".custom-carousel .item").click(function() {
-    $(".custom-carousel .item").not($(this)).removeClass("active");
-    $(this).toggleClass("active");
+      $(".custom-carousel .item").not(this).removeClass("active");
+      $(this).toggleClass("active");
   });
 });
