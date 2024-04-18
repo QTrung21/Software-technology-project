24523fa5a1f43d1fd261627015711ae49f7434a1
Nguyen Cong Hoan <157735901+HoanTheBoy@users.noreply.github.com>
2024-04-18 21:21:46 +0700

    update_game_main

--- Team code/js/game_main.js
+++ Team code/js/game_main.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-  // Initialize the Owl Carousel
   var owl = $('.custom-carousel').owlCarousel({
       autoWidth: true,
       loop: true,
@@ -8,17 +7,14 @@ $(document).ready(function() {
       margin: 10
   });
 
-  // Event listener for custom previous button
   $('.owl-prev').click(function() {
       owl.trigger('prev.owl.carousel');
   });
 
-  // Event listener for custom next button
   $('.owl-next').click(function() {
       owl.trigger('next.owl.carousel');
   });
 
-  // Add double click event to open links in a new tab
   $('.custom-carousel .item').dblclick(function() {
       var link = $(this).data('link');
       if (link) {
@@ -26,7 +22,6 @@ $(document).ready(function() {
       }
   });
 
-  // Add click event to toggle 'active' class
   $(".custom-carousel .item").click(function() {
       $(".custom-carousel .item").not(this).removeClass("active");
       $(this).toggleClass("active");
