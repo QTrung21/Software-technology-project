c0d77216e2f62fd6680524604380d075b7e1c22c
Nguyen Cong Hoan <157735901+HoanTheBoy@users.noreply.github.com>
2024-04-18 21:39:53 +0700

    format lai game

--- Team code/js/game_main.js
+++ Team code/js/game_main.js
@@ -1,29 +1,29 @@
-$(document).ready(function() {
-  var owl = $('.custom-carousel').owlCarousel({
-      autoWidth: true,
-      loop: true,
-      dots: false,
-      nav: true, 
-      margin: 10
+$(document).ready(function () {
+  var owl = $(".custom-carousel").owlCarousel({
+    autoWidth: true,
+    loop: true,
+    dots: false,
+    nav: true,
+    margin: 10,
   });
 
-  $('.owl-prev').click(function() {
-      owl.trigger('prev.owl.carousel');
+  $(".owl-prev").click(function () {
+    owl.trigger("prev.owl.carousel");
   });
 
-  $('.owl-next').click(function() {
-      owl.trigger('next.owl.carousel');
+  $(".owl-next").click(function () {
+    owl.trigger("next.owl.carousel");
   });
 
-  $('.custom-carousel .item').dblclick(function() {
-      var link = $(this).data('link');
-      if (link) {
-          window.open(link, '_blank');
-      }
+  $(".custom-carousel .item").dblclick(function () {
+    var link = $(this).data("link");
+    if (link) {
+      window.open(link, "_blank");
+    }
   });
 
-  $(".custom-carousel .item").click(function() {
-      $(".custom-carousel .item").not(this).removeClass("active");
-      $(this).toggleClass("active");
+  $(".custom-carousel .item").click(function () {
+    $(".custom-carousel .item").not(this).removeClass("active");
+    $(this).toggleClass("active");
   });
 });
