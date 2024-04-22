01050801080c6392f3a1af86cb70023ae83f91a9
edoC510 <nguyenducdong55555@gmail.com>
2024-04-23 04:38:52 +0700

    update news.js

--- Team code/js/news.js
+++ Team code/js/news.js
@@ -1,9 +1,13 @@
+// Chờ web load đầy đủ
 document.addEventListener("DOMContentLoaded", function() {
+  // Lấy tham chiếu đến nút "Xem Thêm" và phần tử chứa tin tức ẩn
   const btnXemThem = document.getElementById("btnXemThem");
   const tinTucHidden = document.querySelector(".tin-tuc.hidden");
 
+  // Gắn sự kiện click vào nút "Xem Thêm"
   btnXemThem.addEventListener("click", function() {
-    tinTucHidden.classList.remove("hidden");
+    // Hiển thị phần tử chứa tin tức ẩn và ẩn nút "Xem Thêm"
+      tinTucHidden.classList.remove("hidden");
     btnXemThem.style.display = "none";
   });
 });
@@ -11,9 +15,11 @@ document.addEventListener("DOMContentLoaded", function() {
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
+  // Nếu vị trí cuộn lớn hơn 20px từ đỉnh của tài liệu, hiển thị nút "Cuộn Lên Đầu Trang"
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
       document.getElementById("scrollBtn").style.display = "block";
   } else {
+      // Ngược lại, ẩn nút "Cuộn Lên Đầu Trang"
       document.getElementById("scrollBtn").style.display = "none";
   }
 }
@@ -23,6 +29,7 @@ function scrollToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+// Cuộn lên mềm mại
 function scrollToTop() {
   window.scrollTo({
       top: 0,
